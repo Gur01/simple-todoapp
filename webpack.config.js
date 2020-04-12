39ef8ab511011279b3a88a8b12c634dcca1599c5
gur01 <vyachgurov@gmail.com>
2020-04-13 05:07:11 +0800

    react ts linter env setup

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: path.join(__dirname, 'src', 'index.tsx')
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 9000
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: '/node_modules/'
+            }
+        ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({template: path.join(__dirname, 'src', 'public', 'index.html')})
+    ]
+}
