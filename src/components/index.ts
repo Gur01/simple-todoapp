3f178cd7c83572104eb844039b85eaf1963faa74
gur01 <vyachgurov@gmail.com>
2020-04-25 15:38:41 +0300

    import fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
-export {default as Spacer} from "./Spacer";
-export {default as Authorization} from "./Authorization";
-export {default as Menu} from "./Menu";
+export { default as Authorization } from "./Authorization";
+export { default as Menu } from "./Menu";
+export { default as Spacer } from "./Spacer";
